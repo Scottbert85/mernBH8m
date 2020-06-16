e88fa45899294ea65e893f635c1188c5f8979bba
Scott Hackman <scottbert85@yahoo.com>
2020-06-16 11:04:03 -0400

    Added profile model.

--- models/Profile.js
+++ models/Profile.js
@@ -1,12 +1,23 @@
-/* 
-  userId
-  fName
-  lName
-  Name
-  City
-  State
-  Avatar
-  githubUrl
-  twitterUrl
-  bio
-*/
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fName: String,
+    lName: String,
+    Name: String,
+    City: String,
+    State: String,
+    Avatar: String, //I assume this is a URL to the avatar and not image data
+    githubUrl: String,
+    twitterUrl: String,
+    bio: String
+  }
+);
+
+module.exports = User = mongoose.model("profile", profileSchema);
